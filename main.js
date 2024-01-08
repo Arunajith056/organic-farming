aa96d805e7e1033b9c786fee06cd5686627f3186
Arunajith056 <arunajith056@gmail.com>
2024-01-08 22:56:19 +0530

    js counter update

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-
-(function ($) {
-    "use strict";
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 40) {
-            $('.navbar').addClass('sticky-top');
-        } else {
-            $('.navbar').removeClass('sticky-top');
-        }
+$(document).ready(function() {
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
     });
-})(jQuery);
+});
